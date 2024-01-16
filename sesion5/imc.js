aec9b83b796303c2dab1fcb59e9d1167c50d100f
JonatanClase85 <156711683+JonatanClase85@users.noreply.github.com>
2024-01-16 19:47:50 +0100

    calculadoraImc

---
+++ sesion5/imc.js
@@ -0,0 +1,12 @@
+document.querySelector('#boton').addEventListener('click', function(){
+    let peso = document.querySelector('#peso').value;
+    let altura = document.querySelector('#altura').value;
+    let imc = CalcularImc(peso, altura);
+    document.querySelector('#calculo').innerHTML = imc;
+})
+
+
+function CalcularImc(peso=0, altura=0) {
+    alturaCuadrado = peso / ( altura ** 2 );
+    return alturaCuadrado;
+}
